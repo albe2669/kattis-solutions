2bc792d98f53e1394019e8461bf68f557a54aad7
albe2669 <albert@risenielsen.dk>
2020-10-03 15:19:31 +0200

    Completed twostones

---
+++ twostones/twostones.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class twostones {
+    public static void main(String[] args) throws Exception {
+        int tc = scan.nextInt();
+        
+        if (tc % 2 == 0) out.println("Bob");
+        else out.println("Alice");
+        
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    
+    public static MyScanner scan = new MyScanner();
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
