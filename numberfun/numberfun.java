9e8a15c6035a656a5df063574dda37a73e81e971
albe2669 <albert@risenielsen.dk>
2020-10-03 16:38:44 +0200

    Completed numberfun

---
+++ numberfun/numberfun.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class numberfun {
+    public static void main(String[] args) throws Exception {
+        int tc = scan.nextInt();
+        for (int i = 0; i < tc; i++) {
+            double a = scan.nextInt();
+            double b = scan.nextInt();
+            double c = scan.nextInt();
+
+            if (
+                a + b == c ||
+                a - b == c ||
+                b - a == c ||
+                a * b == c ||
+                a / b == c ||
+                b / a == c
+            ) {
+                out.println("Possible");
+            } else {
+                out.println("Impossible");
+            }
+        }
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    
+    public static MyScanner scan = new MyScanner();
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
