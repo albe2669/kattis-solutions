d0779e9e6ae991febe8541ab564e01c3b2001748
albe2669 <albert@risenielsen.dk>
2020-10-03 16:56:36 +0200

    Completed modulo

---
+++ modulo/modulo.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+
+public class modulo {
+    public static void main(String[] args) throws Exception {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int s = scan.nextInt() % 42;
+
+            if (!set.contains(s)) set.add(s);
+        }
+
+        out.println(set.size());
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    
+    public static MyScanner scan = new MyScanner();
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
