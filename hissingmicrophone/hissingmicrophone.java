7254f540a51643987cb28a58baea76f57f8e2807
albe2669 <albert@risenielsen.dk>
2020-10-03 14:59:20 +0200

    Completed hissingmicrophone

---
+++ hissingmicrophone/hissingmicrophone.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class hissingmicrophone {
+    public static void main(String[] args) throws Exception {
+        String s = scan.nextLine();
+        char[] l = s.toCharArray();
+        boolean hiss = false;
+        
+        for (int i = 0; i < l.length - 1; i++) {
+            if (l[i] == 's' && l[i+1] == 's') {
+                out.println("hiss");
+                hiss = true;
+                break;
+            }
+        }
+
+        if (!hiss) {
+            out.println("no hiss");
+        }
+
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    
+    public static MyScanner scan = new MyScanner();
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
