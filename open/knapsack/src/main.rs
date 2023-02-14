6b131237582aee4826cea37c107e48a2d214e855
Albert Rise Nielsen <albert@risenielsen.dk>
2023-02-15 00:07:24 +0100

    Another non functioning knapsack solution

--- open/knapsack/src/main.rs
+++ open/knapsack/src/main.rs
@@ -1,81 +1,71 @@
-use std::collections::HashMap;
-use std::io;
+use std::cmp::max;
+use std::io::{self, BufRead};
 
-fn get_input(io: &std::io::Stdin) -> String {
-    let mut buffer = String::new();
-    io.read_line(&mut buffer).expect("Failed");
-    buffer
-}
+fn solve(values: Vec<usize>, weights: Vec<usize>, n: usize, capacity: usize) {
+    let mut knapsack = vec![vec![0; n + 1]; capacity + 1];
 
-fn try_get_input(io: &std::io::Stdin) -> Option<String> {
-    let mut buffer = String::new();
-    match io.read_line(&mut buffer) {
-        Ok(_) => Some(buffer),
-        Err(_) => None,
-    }
-}
-
-fn solve(objects: Vec<(u64, u64)>, capacity: u64) {
-    let mut solutions: HashMap<u64, (u64, Vec<String>)> = HashMap::new();
-    solutions.insert(0, (0, Vec::new()));
-
-    for (index, (obj_value, obj_weight)) in objects.iter().enumerate() {
-        for (weight, (value, indexes)) in solutions.clone().iter() {
-            let new_weight = weight + obj_weight;
-            if new_weight > capacity {
+    for i in 1..n {
+        for w in 1..capacity {
+            if w < weights[i] {
+                knapsack[i][w] = knapsack[i - 1][w];
                 continue;
             }
 
-            let new_value = value + obj_value;
-            let existing = solutions
-                .get(&new_weight)
-                .unwrap_or(&(0, Vec::new()))
-                .to_owned();
-
-            if existing.0 < new_value {
-                let mut new_indexes = indexes.clone();
-                new_indexes.push(index.to_string());
-                solutions.insert(new_weight, (new_value, new_indexes));
-            }
+            knapsack[i][w] = max(
+                values[i] + knapsack[i - 1][w - weights[i]],
+                knapsack[i - 1][w],
+            );
         }
     }
 
-    let mut max_value = 0;
-    let mut max_indexes = Vec::new();
-    for (_, (value, indexes)) in solutions.iter() {
-        if *value > max_value {
-            max_value = *value;
-            max_indexes = indexes.clone();
+    let mut res = knapsack[n][capacity];
+    let mut items = Vec::new();
+
+    let mut weight = capacity;
+    let mut i = n;
+
+    while i > 0 && res > 0 {
+        if res != knapsack[i - 1][weight] {
+            items.push(i.to_string());
+            res -= values[i - 1];
+            weight -= weights[i - 1];
         }
+        i -= 1;
     }
 
-    println!("{}", max_indexes.len());
-    println!("{}", max_indexes.join(" "));
+    println!("{}", items.len());
+    println!("{}", items.join(" "));
 }
 
 fn main() {
-    let stdin = io::stdin();
+    let mut stdin = io::stdin().lock().lines();
 
-    while let Some(cn) = try_get_input(&stdin) {
-        if cn.trim() == "" {
-            break;
-        }
-        let cn = cn
+    while let Some(line) = stdin.next() {
+        let cn = line
+            .unwrap()
             .split_whitespace()
-            .map(|x| x.parse::<u64>().unwrap())
-            .collect::<Vec<u64>>();
-        let capacity = cn[0];
-        let n = cn[1] as usize;
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
 
-        let mut objects = Vec::new();
-        for _ in 0..n {
-            let obj: Vec<u64> = get_input(&stdin)
+        let capacity = cn[0];
+        let n = cn[1];
+
+        let mut weights = vec![0, n + 1];
+        let mut values = vec![0, n + 1];
+
+        for i in 1..n {
+            let obj: Vec<usize> = stdin
+                .next()
+                .unwrap()
+                .unwrap()
                 .split_whitespace()
-                .map(|x| x.parse::<u64>().unwrap())
+                .map(|x| x.parse::<usize>().unwrap())
                 .collect();
-            objects.push((obj[0], obj[1]));
+
+            values[i] = obj[0];
+            weights[i] = obj[1];
         }
 
-        solve(objects, capacity);
+        solve(values, weights, n, capacity);
     }
 }
