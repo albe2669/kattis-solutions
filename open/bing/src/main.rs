9129898cbe9d2c45e1a9ff3c8e0b18433d7ec81a
Albert Rise Nielsen <albert@risenielsen.dk>
2023-03-28 15:00:40 +0200

    Make bing faster

--- open/bing/src/main.rs
+++ open/bing/src/main.rs
@@ -1,17 +1,10 @@
-use std::{
-    collections::HashMap,
-    hash::Hash,
-    io::{self, Read, Write},
-    ops::Add,
-};
+use std::io::{self, Read, Write};
 
-type Input = String;
-type Key = char;
 type Value = u32;
 
 #[derive(Default, Debug)]
 struct Node {
-    children: HashMap<Key, Node>,
+    children: [Option<Box<Node>>; 26],
     value: Value,
 }
 
@@ -26,45 +19,39 @@ impl Trie {
             root: Node::default(),
         }
     }
-
-    pub fn insert(&mut self, value: Input) -> Value {
-        let mut node = &mut self.root;
-        for c in value.chars() {
-            node = node.children.entry(c).or_default();
-            node.value += 1; // Added as a modification
-        }
-
-        node.value - 1 // Added as a modification
-    }
-
-    pub fn get(&self, key: Input) -> Option<&Value> {
-        let mut node = &self.root;
-        for c in key.chars() {
-            if node.children.contains_key(&c) {
-                node = node.children.get(&c).unwrap();
-            } else {
-                return None;
-            }
-        }
-
-        Some(&node.value)
-    }
 }
 
 fn main() {
     let mut buffer = String::new();
     io::stdin().lock().read_to_string(&mut buffer).unwrap();
-    let mut input = buffer.split_whitespace();
+    let (ns, rest) = buffer.split_once('\n').unwrap();
 
     let stdout = io::stdout().lock();
     let mut output = io::BufWriter::new(stdout);
 
-    let n: u32 = input.next().unwrap().trim().parse().unwrap();
+    let n: u32 = ns.trim().parse().unwrap();
+    let mut chars = rest.chars();
 
     let mut trie = Trie::new();
     for _ in 0..n {
-        let instring = input.next().unwrap().trim().to_string();
-        let value = trie.insert(instring);
-        writeln!(output, "{}", value).unwrap();
+        let mut node = &mut trie.root;
+
+        for nchar in chars.by_ref() {
+            if nchar == '\n' {
+                break;
+            }
+
+            match node.children[nchar as usize - 97] {
+                Some(ref mut n) => node = n,
+                None => {
+                    node.children[nchar as usize - 97] = Some(Box::default());
+                    node = node.children[nchar as usize - 97].as_mut().unwrap();
+                }
+            }
+
+            node.value += 1;
+        }
+
+        writeln!(output, "{}", node.value - 1).unwrap();
     }
 }
